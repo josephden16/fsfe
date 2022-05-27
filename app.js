0febe89a32b11edb323ae0beff960ae4b3157cf4
Denedo Oghenetega Joseph <tegadev01@gmail.com>
2022-05-27 16:20:24 +0000

    Add bad route

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/bad-route", (req, res) => {
+  res.status(418);
+  res.set({
+    "X-CUSTOM-HEADER": "Hello World",
+  });
+  res.end();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
